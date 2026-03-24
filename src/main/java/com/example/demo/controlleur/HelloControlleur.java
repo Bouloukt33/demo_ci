a36d98128e414781af4fbfdd50b792f8c1af6c6a
Isma450 <votre.email@example.com>
2026-03-24 15:16:16 +0100

    add workflows github action yml

--- src/main/java/com/example/demo/controlleur/HelloControlleur.java
+++ src/main/java/com/example/demo/controlleur/HelloControlleur.java
@@ -1,6 +1,12 @@
 package com.example.demo.controlleur;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloControlleur {
+
+    @GetMapping("/")
     public String sayHello() {
         return "hello CI/CD Devops de M1 Boc";
     }
